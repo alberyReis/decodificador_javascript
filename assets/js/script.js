e2d5ddbbb624ab1ec3f24a830e3c0705b85f12d5
Perkles <albery.reis.88@gmail.com>
2023-05-19 01:17:41 -0300

    v.1.0

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+const btnCriptografar = document.querySelector('.button_crip')
+const btnDescriptografar = document.querySelector('.button_desc')
+const btnCopiar = document.querySelector('.copiar')
+const input = document.querySelector('#input_text')
+const containerResultado = document.querySelector('#container_resultado')
+
+const criptografar = () => {
+    const inputCriptografar = input.value
+    const textCriptografado = inputCriptografar.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat")
+    containerResultado.innerHTML = ''
+    containerResultado.innerHTML = '<textarea name="" id="texto_resultado" readonly cols="25" rows="40">' + textCriptografado + '</textarea>'
+}
+
+const descriptografar = () => {
+    const inputDescriptografar = input.value;
+    const textDescriptografado = inputDescriptografar.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u")
+    containerResultado.innerHTML = ''
+    containerResultado.innerHTML = '<textarea name="" id="texto_resultado" readonly cols="25" rows="40">' + textDescriptografado + '</textarea>'
+}
+
+const copiar = () => {
+    let textoCopiado = document.getElementById('texto_resultado')
+    textoCopiado.select()
+    document.execCommand('copy')
+    textoCopiado.innerHTML = ''
+}
+
+btnCriptografar.addEventListener('click', (event) => {
+    criptografar()
+})
+
+btnDescriptografar.addEventListener('click', (event) => {
+    descriptografar()
+})
+
+btnCopiar.addEventListener('click', (event) => {
+    copiar()
+})
+
+
+
